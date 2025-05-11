c18cc641ea5d24868a1992ad755e4c14c8b271d2
luongkhiemdu <138788016+luongkhiemdu@users.noreply.github.com>
2025-05-11 17:03:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var randomNumber1 = Math.floor(Math.random()*6) + 1 //1-6
 var randomNumber2 = Math.floor(Math.random()*6) + 1 //1-6
 var randomDiceImage1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png     
 var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
-var randomImageSource1 = "images/" + randomDiceImage1; //images/dice1.png - images/dice6.png
-var randomImageSource2 = "images/" + randomDiceImage2; //images/dice1.png - images/dice6.png    
+var randomImageSource1 = "./images/" + randomDiceImage1; //images/dice1.png - images/dice6.png
+var randomImageSource2 = "./images/" + randomDiceImage2; //images/dice1.png - images/dice6.png    
 var image1 = document.querySelectorAll("img")[0]; //selects the first image
 var image2 = document.querySelectorAll("img")[1]; //selects the second image        
 image1.setAttribute("src", randomImageSource1); //changes the image source to the random image
